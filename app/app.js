4ecfb679159b63299a7204708e75d93dd320ae0a
Ryan Thomas <rrt2335@gmail.com>
2019-02-05 11:45:46 -0700

    Debugger implementation

--- app/app.js
+++ app/app.js
@@ -16,10 +16,17 @@ var people = [{
 }]
 
 /*
+let me = {
+    name: "Ryan",
+    age: 31,
+    food: "pasta",
+    hair: true,
+    eyes: true,
+}
+
 console.log(`Hello, my name is ${people[0].name} and my favorite food is ${people[0].food}`);
 console.log(`Hello, my name is ${people[0].name} and my favorite food is ${people[0].food}`);
 console.log(`Hello, my name is ${people[0].name} and my favorite food is ${people[0].food}`);
-*/
 
 // 'FOR' LOOP
 for (let i = 0; i < people.length; i++) {
@@ -37,6 +44,14 @@ people.forEach(person => {
 })
 console.log("\'For-Each\' Loop completed!");
 
+// 'FOR-IN' LOOP
+people.forEach(person => {
+    for (let key in me) {
+        console.log(key +":", me[key]);
+    }
+    console.log("_____________");
+})
+
 // 'WHILE' loop
 let i = 0;
 while (i < people.length) {
@@ -52,22 +67,31 @@ do {
 } while (false);
 console.log("\'Do-While\' Loop completed!");
 
-// Filter returns an array of items that meet the condition
+// FILTER array method returns an array of items that meet the condition
 let pastaLovers = people.filter(person => {
     let personVar = person;
     return person.food == "pasta"
 })
 console.log(pastaLovers);
 
-// Find returns the first element from the array that meets the condition
+// FIND array method returns the first element from the array that meets the condition
 let foundPerson = people.find(person => {
     return (person.food.toLowerCase() == "pasta");
 })
 console.log(foundPerson);
 
-// Map iterates over the array and runs the function whatever is returned replaces each element
+// MAP array method iterates over the array and runs the function whatever is returned replaces each element
 let justNames = people.map(person => person.name);
 
 let nums = [1,2,3,4,5,6];
 let doubleNums = nums.map(num => num*2);
 console.log( doubleNums);
+*/
+
+let template = ""
+people.forEach(person => {
+   // debugger // Execution stops here, and is paused line by line for debugging
+    template += `<li>${person.name} likes ${person.food}</li>`
+    console.log(template);
+})
+document.getElementById('staff').innerHTML = template
